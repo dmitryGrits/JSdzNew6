c2b7d7e2ce2547eb60e18f57f70185772a0711ef
DGRITS <grizay@icloud.com>
2024-03-19 15:50:42 +0300

    1 commit

---
+++ data.js
@@ -0,0 +1,25 @@
+const data = `
+[
+    {
+        "title": "MANGO PEOPLE T-SHIRTS",
+        "price": "300",
+        "color": "red",
+        "size": "XL",
+        "image": "https://sizcentr.ru/upload/iblock/9b4/9b4c2d153b26e3c2ceaff98bde3bcc5b.png"
+    },
+    {
+        "title": "ELLEXY X MO CAPSULE",
+        "price": "52",
+        "color": "black",
+        "size": "XL",
+        "image": "https://sevengift.ru/images/thumbnails/1374.30_13_tif_1000x1000.jpg"
+    },
+    {
+        "title": "SOME T-SHIRT",
+        "price": "500",
+        "color": "green",
+        "size": "L",
+        "image": "https://pechati22.ru/wp-content/uploads/2022/07/8023_3415c6.jpg"
+    }
+]
+`;
